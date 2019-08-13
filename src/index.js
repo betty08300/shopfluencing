efd90b95d2d7e73b87283fe34997d9db7a9cca64
Betty Wren <wreny641@gmail.com>
2019-08-13 16:45:11 -0400

    create data dictionary

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@
 
 
 const result = {}
+
 d3.csv('/data/shopfluencing.csv').then(data => {
     data.forEach((row, index) => {
         let segmentType = row['Segment Type'];
+        let segmentDescription = row['Segment Description'];
+        let platform = row['Platform']
         // this is a shortcut to check if result has the segmentType key already
         // this means use the same value if it not undefined. if it is undefined, 
         // that means we have never set it before, so start it with an empty {}
         result[segmentType] = result[segmentType] || {};
+        result[segmentType][segmentDescription] = result[segmentType][segmentDescription] || {}
+        result[segmentType][segmentDescription][platform] = row['Count']
+        
+        
+    
     })
     console.log(result);
+    
+    
+    
+
 })
 
 
