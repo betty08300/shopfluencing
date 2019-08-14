79b4b0f7f3ef06f556876cc8e9a85d91e5594d69
Betty Wren <wreny641@gmail.com>
2019-08-14 15:30:40 -0400

    working on pie chart

--- src/index.js
+++ src/index.js
@@ -32,6 +32,8 @@ d3.csv('/data/shopfluencing.csv').then(data => {
     buildRightCharts(ethnicityData)
 
     // TODO: build middle chart
+
+    buildPieCharts(platform)
     
 })
 
@@ -131,4 +133,47 @@ let handleSegmentClick = function(segmentsData) {
     d3.select(".right-side").selectAll('svg').remove();
     // Rebuild charts for segmentsData
     buildRightCharts(segmentsData)
+}
+
+let buildPieCharts = (platformData) => {
+    for (let platform in platformData){
+        buildPieChart(platformData[platform])
+    }
+}
+
+let buildPieChart = function (platform){
+    const arcs = pie(platform);
+
+    const svg = d3.create("svg")
+        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+    svg.append("g")
+        .attr("stroke", "white")
+        .selectAll("path")
+        .data(arcs)
+        .join("path")
+        .attr("fill", d => color(d.platform.name))
+        .attr("d", arc)
+        .append("title")
+        .text(d => `${d.platform.name}: ${d.platform.value.toLocaleString()}`);
+
+    svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 12)
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(platform)
+        .join("text")
+        .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+        .call(text => text.append("tspan")
+            .attr("y", "-0.4em")
+            .attr("font-weight", "bold")
+            .text(d => d.platform.name))
+        .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+            .attr("x", 0)
+            .attr("y", "0.7em")
+            .attr("fill-opacity", 0.7)
+            .text(d => d.platform.value.toLocaleString()));
+
+    return svg.node();
 }
